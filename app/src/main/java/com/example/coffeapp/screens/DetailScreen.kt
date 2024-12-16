a955cee0b8982fe597d9707895d25b68684eaf2e
Alexkezz <alex391969@gmail.com>
2024-12-16 20:39:28 +0100

    Addes scroll detailScreen

--- app/src/main/java/com/example/coffeapp/screens/DetailScreen.kt
+++ app/src/main/java/com/example/coffeapp/screens/DetailScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
@@ -58,6 +60,7 @@ fun DetailScreen(
     var inputTitle by remember { mutableStateOf(title ?: "") }
     var inputDescription by remember { mutableStateOf(description ?: "") }
     var inputIngredients by remember { mutableStateOf(ingredients ?: "") }
+    val scrollState = rememberScrollState()
 
     fun onBackClick() {
         navController.popBackStack()
@@ -81,7 +84,7 @@ fun DetailScreen(
             modifier = Modifier
                 .padding(paddingValues = it)
                 .padding(10.dp)
-
+                .verticalScroll(scrollState)
         ) {
             Text(
                 text = title ?: "No title Coffee",
